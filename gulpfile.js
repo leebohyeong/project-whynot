9b03b0432d8fc213f333a4bff0222146493d5f9d
Byeonghwa.Yeon <hwalybell@groupidd.com>
2022-05-30 14:32:35 +0900

    이벤트 제작

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-sass')(require('sass'));
+const babel = require('gulp-babel');
+const pug = require('gulp-pug-3');
+const autoprefixer = require('gulp-autoprefixer');
+const cssimport = require('gulp-cssimport');
+const concat = require('gulp-concat');
+const argv = require('yargs').argv;
+const connect = require('gulp-connect');
+
+const src = './src';
+const dest = './dest';
+
+const options = (() => {
+    return {
+        run: argv['run']
+    };
+})();
+
+gulp.task('server', () => {
+    connect.server({
+        port: 8000,
+        root: 'dest',
+        livereload: true
+    });
+});
+
+gulp.task('html', () => {
+    return gulp.src([
+
+        `${src}/views/web/**/[^_]*.pug`
+    ])
+        .pipe(pug({
+            pretty: false,
+        }))
+        .pipe(gulp.dest(`${dest}/`));
+});
+
+gulp.task('js', () => {
+    return gulp.src([
+        `${src}/js/web/*.js`
+    ])
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(`${dest}/assets/js`));
+});
+
+gulp.task('css', () => {
+    return gulp.src(`${src}/scss/web/[^_]*.?(s)css`)
+        .pipe(sass({
+            indentWidth: 4,
+            outputStyle: 'expanded',
+            // outputStyle: 'compressed',
+            // outputStyle: 'compact',
+            includePaths: [
+                path.resolve(__dirname, 'node_modules')
+            ]
+        }))
+        .pipe(autoprefixer())
+        .pipe(cssimport())
+        .pipe(gulp.dest(`${dest}/assets/css`));
+});
+
+const runMethods = {
+    'server'() {
+        gulp.task('default', gulp.series(['css', 'js', 'html', 'server']));
+        gulp.watch(`${src}/views/web/**/[^_]*.pug`, gulp.series(['html']));
+        gulp.watch(`${src}/scss/web/**/[^_]*.?(s)css`, gulp.series(['css']));
+        gulp.watch(`${src}/js/web/*.js`, gulp.series(['js']));
+    },
+};
+
+runMethods[`${options.run}`]();
---
+++ src/js/web/app.js
@@ -0,0 +1,43 @@
+const Modal = class {
+    constructor(modals) {
+        this.body = document.body;
+        this.modals = modals;
+        this.currentIndex = 0;
+
+        this.initEvents();
+    }
+
+    open(index) {
+        this.body.classList.add('modal-open');
+        this.modals[index].classList.add('modal--open');
+        this.currentIndex = index;
+    }
+
+    close() {
+        this.body.classList.remove('modal-open');
+        this.modals[this.currentIndex].classList.remove('modal--open');
+    }
+
+    initEvents() {
+        this.modals.forEach(modal => {
+            const backdrop = modal.querySelector('.modal__backdrop');
+
+            backdrop.addEventListener('click', (event) => {
+                this.close();
+            });
+        });
+    }
+};
+
+const app = () => {
+    const zone = document.querySelector('.why-not__zone');
+    const triggers = [...zone.querySelectorAll('a')];
+    const details = triggers.map(trigger => document.querySelector(trigger.getAttribute('href')));
+    const modal = new Modal(details);
+
+    triggers.forEach((trigger, index) => {
+        trigger.addEventListener('click', () => modal.open(index));
+    });
+};
+
+document.addEventListener('DOMContentLoaded', app);
