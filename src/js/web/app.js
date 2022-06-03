455cb8af61dccfaeaf4ae842c694a41423a2b0fb
Byeonghwa.Yeon <hwalybell@groupidd.com>
2022-06-03 12:03:22 +0900

    수정

--- src/js/web/app.js
+++ src/js/web/app.js
@@ -30,17 +30,69 @@ const Modal = class {
 };
 
 const app = () => {
-    const zone = document.querySelector('.why-not__zone');
-    const triggers = [...zone.querySelectorAll('a')];
-    const details = triggers.map(trigger => document.querySelector(trigger.getAttribute('href')));
-    const modal = new Modal(details);
+    (() => {
+        const url = new URL(location);
+        const nav = document.querySelector('.why-not-nav');
+        const triggers = [...nav.querySelectorAll('a')];
+        const navNames = triggers.map(trigger => trigger.getAttribute('href').replace('/?content=', ''));
+        const paramIndex = navNames.indexOf(url.searchParams.get('content'));
+        const current = paramIndex !== -1 ? navNames[paramIndex] : navNames[0];
+        const article = document.getElementById(current);
 
-    triggers.forEach((trigger, index) => {
-        trigger.addEventListener('click', (event) => {
-            event.preventDefault();
-            modal.open(index);
+        nav.classList.add(`why-not-nav--${current}`);
+        article.classList.add(`${article.classList[0]}--active`);
+    })();
+
+    (() => {
+        const nav = document.querySelector('.why-not-nav');
+        const triggers = [...nav.querySelectorAll('a')];
+        const articles = triggers.reduce((articles, trigger) => {
+            const id = trigger.getAttribute('href').replace('/?content=', '');
+            const article = document.getElementById(id);
+            const movies = article.querySelectorAll('iframe');
+
+            articles[id] = {
+                element: document.getElementById(id),
+                movies: movies.length ? [...movies] : []
+            };
+
+            return articles;
+        }, {});
+        const toggleNav = (id) => {
+            nav.classList.remove('why-not-nav--why-not', 'why-not-nav--why-not-load');
+            nav.classList.add(`why-not-nav--${id}`);
+        };
+        const toggleArticle = (id) => {
+            Object.keys(articles).forEach(articleName => {
+                const {element, movies} = articles[articleName];
+
+                element.classList[articleName === id ? 'add' : 'remove'](`${element.classList[0]}--active`);
+                movies.forEach(movie => movie.src = movie.src);
+            });
+        };
+        triggers.forEach((trigger, index) => {
+            trigger.addEventListener('click', (event) => {
+                const id = trigger.getAttribute('href').replace('/?content=', '');
+
+                toggleNav(id);
+                toggleArticle(id);
+            });
         });
-    });
+    })();
+
+    (() => {
+        const zone = document.querySelector('.why-not-load__zone');
+        const triggers = [...zone.querySelectorAll('a')];
+        const details = triggers.map(trigger => document.querySelector(trigger.getAttribute('href')));
+        const modal = new Modal(details);
+
+        triggers.forEach((trigger, index) => {
+            trigger.addEventListener('click', (event) => {
+                event.preventDefault();
+                modal.open(index);
+            });
+        });
+    })();
 };
 
 document.addEventListener('DOMContentLoaded', app);
