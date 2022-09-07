25f2a9a738ba8dea1a28806903c92923fea52947
Byeonghwa.Yeon <hwalybell@groupidd.com>
2022-09-07 11:33:43 +0900

    영상 링크 수정

--- __resource/js/pc/app.js
+++ __resource/js/pc/app.js
@@ -1,15 +1,62 @@
+import AOS from 'aos';
+import sal from 'sal.js'
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import {findOne, getOffset, on} from '../helper';
+import {findOne, find, getOffset, on} from '../helper';
+import Tab from '../Tab';
+
 gsap.registerPlugin(ScrollTrigger);
 
-const Tab = class {
-    constructor(element) {
-
-    }
-};
-
 const app = () => {
+    AOS.init({
+        once: true
+    });
+
+    // ScrollSpy
+    (() => {
+        const header = findOne('.header');
+        const headerHeight = header.clientHeight;
+        const links = find('.header__link', header);
+        const sections = links.map(link => findOne(link.getAttribute('href')));
+        const sectionsStart = [];
+        const getSectionsStart = () => sections.forEach((section, index) => sectionsStart[index] = ~~(getOffset(section).top - headerHeight));
+        const toggleLink = () => {
+            const scrollY = window.scrollY;
+            let currentIndex = sectionsStart.length - 1;
+
+            while (currentIndex) {
+                if (scrollY >= sectionsStart[currentIndex]) break;
+
+                currentIndex--;
+            }
+
+            links.forEach((link, index) => {
+                link.classList[index === currentIndex ? 'add' : 'remove']('header__link--active');
+            });
+        };
+        const moveSection = (index) => {
+            window.scroll({
+                top: sectionsStart[index],
+                behavior: 'smooth'
+            });
+        };
+
+        getSectionsStart();
+
+        links.forEach((link, index) => {
+            on(link, 'click', (event) => {
+                event.preventDefault();
+                moveSection(index);
+                toggleLink();
+            });
+        });
+
+        on(window, 'load', getSectionsStart);
+        on(window, 'resize', getSectionsStart);
+        on(window, 'scroll', toggleLink);
+    })();
+
+    // WHY NOT?
     (() => {
         const whyNot = findOne('.why-not');
         const wrap = findOne('.why-not__wrap', whyNot);
@@ -71,7 +118,7 @@ const app = () => {
 
         reset();
         getScene();
-        wrap.classList.add(`hy-not__wrap--scene-${info.currentScene}`);
+        wrap.classList.add(`why-not__wrap--scene-${info.currentScene}`);
 
         let scrollY = window.scrollY;
         let currentScene = 0;
@@ -82,16 +129,64 @@ const app = () => {
                 const {isChangeScene, prevScene, currentScene} = info;
 
                 if (isChangeScene) {
-                    wrap.classList.remove(`hy-not__wrap--scene-${prevScene}`);
-                    wrap.classList.add(`hy-not__wrap--scene-${currentScene}`);
+                    wrap.classList.remove(`why-not__wrap--scene-${prevScene}`);
+                    wrap.classList.add(`why-not__wrap--scene-${currentScene}`);
+
+                    if (currentScene === 0) {
+                        scene1();
+                    }
                 }
             }
             // scrollY = window.scrollY;
             // enterNewScene = false;
         };
+        const scene1 = (() => {
+            const movie = findOne('.why-not__scene-1 video');
+            on(movie, 'timeupdate', () => {
+                if (movie.currentTime > 5) {
+                    movie.pause();
+                }
+            });
+            return () => {
+                movie.currentTime = 0;
+                movie.play();
+            }
+        })();
 
         on(window, 'scroll', onMotion);
     })();
+
+    // Brand Film
+    (() => {
+        const brandFilm = findOne('.brand-film');
+        const tab = new Tab(findOne('.tab', brandFilm));
+        tab.menus[0].click();
+    })();
+
+    // 유독
+    (() => {
+        const pogg = findOne('.pogg');
+
+        pogg.addEventListener('sal:in', ({ detail }) => {
+            console.log('animated in', detail);
+        });
+
+        pogg.addEventListener('sal:out', ({ detail }) => {
+            console.log('animated out', detail);
+        });
+    })();
+
+    // document.addEventListener('sal:out', ({ detail }) => {
+    //     console.log('exiting', detail.target);
+    // });
+
+    // WHY NOT 크루
+    (() => {
+        const whyNotCrew = findOne('.why-not-crew');
+        const tab = new Tab(findOne('.tab', whyNotCrew));
+        tab.menus[0].click();
+    })();
+
 
 
 };
