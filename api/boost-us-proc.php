d2825d64c90c6637520408ddeed45613d8e1a966
Dongim.Kim <mojito@groupidd.com>
2023-04-11 18:02:03 +0900

    수정

--- api/boost-us-proc.php
+++ api/boost-us-proc.php
@@ -36,16 +36,39 @@ $requestInfo = array(
     'concept' => $_POST['concept'] ?? '',
     'reason' => $_POST['reason'] ?? '',
     'agree1' => $_POST['agree1'] ?? '',
-    'agree1_ver' => $_POST['agree1_ver'] ?? '',
+    'agree1_ver' => $_POST['agree1_ver'] ?? '1.0',
     'agree2' => $_POST['agree2'] ?? '',
-    'agree2_ver' => $_POST['agree2_ver'] ?? '',
+    'agree2_ver' => $_POST['agree2_ver'] ?? '1.0',
+    'device' => $DEVICE_TYPE_,
     'user_ip' => $USER_IP_                                                   // 사용자 아이피
 );
+
+$requestInfo['name']    = '김동임';
+$requestInfo['comm_id']    = 'KTF';     // SKT, LGT, KTF, 알뜰폰 : SKM, LGM, KTM
+$requestInfo['hphone']    = '01027897151';
+$requestInfo['birthday']    = '19810518';
+$requestInfo['channel']    = 'youtube';
+$requestInfo['url']    = 'http://www.naver.com';
+$requestInfo['concept']    = '컨셉';
+$requestInfo['reason']    = '지원이유';
+$requestInfo['agree1']    = 'Y';
+$requestInfo['agree1_ver']    = '1.0';
+$requestInfo['agree2']    = 'Y';
+$requestInfo['agree1_ver']    = '2.0';
+
 
 $validator = new Validator($requestInfo);
 $validator->rule('required', 'name')->message('이름 정보가 올바르지 않습니다.');
 $validator->rule('required', 'hphone')->message('연락처 정보가 올바르지 않습니다.');
-//$validator->rule('length', 'event_key', 15)->message('로그인 정보가 올바르지 않습니다.');
+$validator->rule('required', 'birthday')->message('생년월일 정보가 올바르지 않습니다.');
+$validator->rule('length', 'birthday', 8)->message('생년월일 정보가 올바르지 않습니다.');
+$validator->rule('required', 'channel')->message('채널 정보가 올바르지 않습니다.');
+$validator->rule('in', 'channel', array('youtube', 'instagram'))->message('채널 정보가 올바르지 않습니다.');
+$validator->rule('required', 'url')->message('url 정보가 올바르지 않습니다.');
+$validator->rule('required', 'agree1')->message('개인정보 수집 및 활용동의를 해주시기 바랍니다.');
+$validator->rule('in', 'agree1', array('Y'))->message('개인정보 수집 및 활용동의를 해주시기 바랍니다.');
+$validator->rule('required', 'agree2')->message('지원/참여자 유의사항에 동의를 해주시기 바랍니다.');
+$validator->rule('in', 'agree2', array('Y'))->message('개인정보 수집 및 활용동의를 해주시기 바랍니다.');
 
 
 if ($validator->validate()) {
@@ -64,14 +87,12 @@ if ($validator->validate()) {
 
 $db = new ModelBase();
 $db->beginTransaction();
-$db->from("EVENT_APPLY");
+$db->from("BOOSTUS_APPLY");
 if ($db->insert($requestInfo)) {
     $event_seq = $db->lastInsertId();
 
     $response['result'] = true;
     $response['message'] = '이벤트에 정상적으로 참여되었습니다.';
-    $response['prize']    = 'N';
-    $response['prize_name']   = '';
 
     echo json_encode($response);
     exit();
